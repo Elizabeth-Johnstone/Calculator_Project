5fe1a4a8ed59423e86a830b02acb4741483f5fb2
Elizabeth Johnstone <ewj218@nyu.edu>
2021-11-24 15:09:38 -0500

    heroku will work now

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
+
+
 const express = require("express");
 const cors = require("cors");
 const { SERVER_PORT } = process.env;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.set('port', (process.env.PORT || 5858));
+
 
 const {
 calculateNums,
