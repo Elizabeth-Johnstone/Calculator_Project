791cb5489749561b837f8a5634266dbf4f4bda49
Elizabeth Johnstone <ewj218@nyu.edu>
2022-01-10 13:21:47 -0500

    included comments in JS fiels

--- client/script.js
+++ client/script.js
@@ -7,22 +7,41 @@ clearButton.addEventListener("click", clearScreen)
 equalsButton.addEventListener("click", calculate)
 themeButton.addEventListener("click", changeTheme)
 
+/*
+display() is used in index.html. When a button with a number or mathematical operator is clicked, the corresponding character is shown on the display screen.
+*/
+
 function display(value) {
     displayScreen.value += value
 }
+/*
+clearScreen() makes the display screen value an empty string. It clears/restarts calculations.
+*/
 
 function clearScreen() {
     displayScreen.value = ""
 }
 
-const baseURL = window.location.origin
-// const baseURL = `http://localhost:5858`
+/*
+Assign baseURL to window.location.origin for Heroku deployment. Assign it to `http://localhost:5858` to run locally.
+*/
+
+// const baseURL = window.location.origin
+const baseURL = `http://localhost:5858`
+
+/*
+calculate() uses a POST request to send user inputted values to the back-end. When calculateNums sends back an answer to the inputted values, calculate() sets the value of displayScreen to that answer.
+*/
 
 function calculate() {
     let body = { inputVals: displayScreen.value }
     axios.post(`${baseURL}/api/display`, body).then((res) => displayScreen.value = res.data)
     .catch((error) => console.log(error))
 }
+
+/*
+changeTheme() performs a GET request to the back-end. The back-end sends back a random theme with generateTheme. Then changeTheme() checks if the theme selected by generateTheme is the current displayed theme. If it is, it goes back and requests another random theme. Otherwise, it sets the new theme to the random theme.
+*/
 
 function changeTheme() {
     axios.get(`${baseURL}/api/theme`)
--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,9 @@
 module.exports = {
+    
+/*
+calculateNums expects a string of two numbers separated by '/', 'x', '-', or '+'. It uses RegEx to split the string on the mathematical operator. Then, it assigns the two numbers to variables and converts them from strings to numbers. Finally, it performs the necessary calculation and sends the answer to the front-end.
+*/
+  
     calculateNums: (req, res) => {
         toCalculate = req.body.inputVals
         numsToCalculate = toCalculate.split(/[-+/x]/)
@@ -17,6 +22,11 @@ module.exports = {
         } else {answer = firstNum}
         res.status(200).send(`${answer}`)
       },
+
+      
+/*
+generateTheme selects a random theme and sends it to the front-end.
+*/
 
     generateTheme: (req, res) => {
         const themes = ['theme-pink', 'theme-blue', 'theme-green', 'theme-orange']
