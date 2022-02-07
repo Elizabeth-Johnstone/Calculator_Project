34ad90900b8a3306e87a67205277c1e5db4c8707
Elizabeth Johnstone <ewj218@nyu.edu>
2022-02-06 22:13:42 -0500

    Updated favicon

--- client/script.js
+++ client/script.js
@@ -1,18 +1,18 @@
-const displayScreen = document.getElementById("display")
-const clearButton = document.getElementById("clear")
-const equalsButton = document.getElementById("equals")
-const themeButton = document.getElementById("theme-button")
+const displayScreen = document.getElementById("display");
+const clearButton = document.getElementById("clear");
+const equalsButton = document.getElementById("equals");
+const themeButton = document.getElementById("theme-button");
 
-clearButton.addEventListener("click", clearScreen)
-equalsButton.addEventListener("click", calculate)
-themeButton.addEventListener("click", changeTheme)
+clearButton.addEventListener("click", clearScreen);
+equalsButton.addEventListener("click", calculate);
+themeButton.addEventListener("click", changeTheme);
 
 /*
 display() is used in index.html. When a button with a number or mathematical operator is clicked, the corresponding character is shown on the display screen.
 */
 
 function display(value) {
-    displayScreen.value += value
+  displayScreen.value += value;
 }
 
 /*
@@ -20,24 +20,26 @@ clearScreen() makes the display screen value an empty string. It clears/restarts
 */
 
 function clearScreen() {
-    displayScreen.value = ""
+  displayScreen.value = "";
 }
 
 /*
 Assign baseURL to window.location.origin for Heroku deployment. Assign it to `http://localhost:5858` to run locally.
 */
 
-const baseURL = window.location.origin
-// const baseURL = `http://localhost:5858`
+// const baseURL = window.location.origin
+const baseURL = `http://localhost:5858`;
 
 /*
 calculate() uses a POST request to send user inputted values to the back-end. When calculateNums sends back an answer to the inputted values, calculate() sets the value of displayScreen to that answer.
 */
 
 function calculate() {
-    let body = { inputVals: displayScreen.value }
-    axios.post(`${baseURL}/api/display`, body).then((res) => displayScreen.value = res.data)
-    .catch((error) => console.log(error))
+  let body = { inputVals: displayScreen.value };
+  axios
+    .post(`${baseURL}/api/display`, body)
+    .then((res) => (displayScreen.value = res.data))
+    .catch((error) => console.log(error));
 }
 
 /*
@@ -45,15 +47,16 @@ changeTheme() performs a GET request to the back-end. The back-end sends back a 
 */
 
 function changeTheme() {
-    axios.get(`${baseURL}/api/theme`)
+  axios
+    .get(`${baseURL}/api/theme`)
     .then(function (res) {
-        let currentTheme = document.documentElement.className
-        if (res.data === currentTheme) {
-            changeTheme()
-        } else {
-        localStorage.setItem('theme', res.data);
-        document.documentElement.className = res.data
-        }
+      let currentTheme = document.documentElement.className;
+      if (res.data === currentTheme) {
+        changeTheme();
+      } else {
+        localStorage.setItem("theme", res.data);
+        document.documentElement.className = res.data;
+      }
     })
-    .catch((error) => console.log(error))
+    .catch((error) => console.log(error));
 }
